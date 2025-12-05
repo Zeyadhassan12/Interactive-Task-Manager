7c8ebffd9c29fad75423bfbf83480e33e35f24c6
Amr Saad <amr.saad@konecta.com>
2025-12-05 09:20:38 -0800

    Getting starting

---
+++ interactive-task-manager/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component, signal } from '@angular/core';
+
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  
+}
