1c4b99b7e4b016ec6245c09749a6aae3a693eae6
Amr Saad <amr.saad@konecta.com>
2025-12-08 08:43:04 -0800

    Implementing the header

--- interactive-task-manager/src/app/app.component.ts
+++ interactive-task-manager/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
+import { Header } from './header/header';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [Header],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ interactive-task-manager/src/app/users_name.ts
@@ -0,0 +1,26 @@
+export const USERS_NAMES = [
+  {
+    id: 'u1',
+    name: 'Jasmine Washington',
+  },
+  {
+    id: 'u2',
+    name: 'Emily Thompson',
+  },
+  {
+    id: 'u3',
+    name: 'Marcus Johnson',
+  },
+  {
+    id: 'u4',
+    name: 'David Miller',
+  },
+  {
+    id: 'u5',
+    name: 'Priya Patel',
+  },
+  {
+    id: 'u6',
+    name: 'Arjun Singh',
+  },
+];
