164dd1234ab173a15519368bf68f0447fdf90f6e
Amr Saad <amr.saad@konecta.com>
2025-12-28 19:45:23 -0800

    Implementing Reactive Forms

--- interactive-task-manager/src/app/tasks/new-task/new-task.component.ts
+++ interactive-task-manager/src/app/tasks/new-task/new-task.component.ts
@@ -1,11 +1,12 @@
 import { Component, EventEmitter, Input, Output, inject, SimpleChanges, OnChanges } from '@angular/core';
 import { TasksService } from '../tasks.service';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Task } from '../task/task.model';
-
+import { CommonModule } from '@angular/common';
+import { Title } from '@angular/platform-browser';
 @Component({
   selector: 'app-new-task',
-  imports: [FormsModule],
+  imports: [ CommonModule, ReactiveFormsModule ],
   standalone: true,
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.css',
@@ -15,46 +16,65 @@ export class NewTaskComponent implements OnChanges {
   @Input() task?: Task;  
   @Output() close = new EventEmitter<void>();
 
-  enteredTitle= '';
-  enteredSummary='';
-  enteredDate='';
+  taskForm : FormGroup;
 
   private tasksService = inject(TasksService);
+  private fb = inject(FormBuilder);
 
+  constructor(){
+    this.taskForm = this.fb.group({
+        title: ["",Validators.required],
+        summary:["",Validators.required],
+        date:["",Validators.required]
+    });
+  }
 
-  // When editing
-  ngOnChanges(changes: SimpleChanges) {
-    if (changes['task'] && this.task) {
-      this.enteredTitle = this.task.title;
-      this.enteredSummary = this.task.summary;
-      this.enteredDate = this.task.dueDate;
-    }
+  ngOnChanges(changes: SimpleChanges){
+      if (changes['task'] && this.task){
+        this.taskForm.patchValue({
+          title: this.task.title,
+          summary:this.task.summary,
+          date:this.task.dueDate
+        });
+      }
+  }
+
+  get title(){
+    return this.taskForm.get('title');
+  }
+
+  get summary(){
+    return this.taskForm.get('summary');
+  }
+
+  get date(){
+    return this.taskForm.get('date');
   }
 
   onCancel(){
+    this.taskForm.reset();
     this.close.emit();
   }
 
-
   onSubmit() {
-    if (this.task) {
-      // Editing existing task
-      this.tasksService.updateTask(this.task.id, {
-        title: this.enteredTitle,
-        summary: this.enteredSummary,
-        dueDate: this.enteredDate,
-      }).subscribe();
-    } else {
-      // Adding new task
-      this.tasksService.addTask(
-        {
-          title: this.enteredTitle,
-          summary: this.enteredSummary,
-          date: this.enteredDate,
-        },
-        this.userId
-      ).subscribe();
+    if (this.taskForm.valid) {
+      const { title, summary, date } = this.taskForm.value;
+
+      if (this.task) {
+        // Edit
+        this.tasksService.updateTask(this.task.id!, {
+          title: title!,
+          summary: summary!,
+          dueDate: date!
+        }).subscribe(() => this.close.emit());
+      } else {
+        // Add
+        this.tasksService.addTask({
+          title: title!,
+          summary: summary!,
+          date: date!
+        }, this.userId).subscribe(() => this.close.emit());
+      }
     }
-    this.close.emit();
   }
 }
